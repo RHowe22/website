112979b1586b2565299b7b2e6bd14200e961c36a
Rob Howe <robjhowe22@gmail.com>
2021-02-12 13:17:39 -0500

    initial commit

---
+++ website/pages/index.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rob Howe's Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <header></header>
+
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hi I'm Rob
+        </h1>
+
+        <h2 className={styles.description}>
+          A little about me
+        </h2>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          see source here
+          <img src="/github.svg" alt="GitHub Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
